66a90271d9956ebb57b239b43d2819ed2fdcd018
Atif Siddiqui <atifsiddiqui35@gmail.com>
2026-07-02 02:44:13 +0530

    Add /jobs/:id detail page with resume generation

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { Navigate, Route, Routes } from "react-router-dom";
 import Layout from "./components/Layout";
 import SearchPage from "./pages/SearchPage";
 import JobsPage from "./pages/JobsPage";
+import JobDetailPage from "./pages/JobDetailPage";
 
 export default function App() {
   return (
@@ -10,7 +11,7 @@ export default function App() {
         <Route index element={<Navigate to="/jobs" replace />} />
         <Route path="search" element={<SearchPage />} />
         <Route path="jobs" element={<JobsPage />} />
-        <Route path="jobs/:id" element={<div>Job detail (Task 7)</div>} />
+        <Route path="jobs/:id" element={<JobDetailPage />} />
         <Route path="jobs/:id/employees" element={<div>Employees (Task 8)</div>} />
         <Route path="jobs/:id/referral" element={<div>Referral (Task 9)</div>} />
       </Route>
